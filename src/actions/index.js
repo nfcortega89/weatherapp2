4dac928e42a4744ca05b84ea3561e9c706a31dbb
Niccolo Ortega <nfcortega89@gmail.com>
2018-03-02 11:42:59 -0800

    created action creators

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const API_KEY = '1007a95226b70d0eb566fa05d6b89cd0';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
